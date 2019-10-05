c9a3879c1a843cfb29cc3e4de21c3e6346fc90bc
Daniel Apodaca <apodacaduron@gmail.com>
2019-10-05 00:25:29 -0600

    feat(404): not found page

---
+++ src/components/404.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="page_404">
+    <div class="container">
+      <h1 class="not-found-title no-margins">404</h1>
+      <span class="subtitle">Still working on this, will be ready soon :)</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'notFound',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+.page_404 {
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  background: #071017;
+  color: #ffffff;
+}
+.container {
+  height: 80%;
+  padding: 0 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.not-found-title {
+  font-size: 14rem;
+}
+@media screen and (max-width: 768px) {
+  .not-found-title {
+    font-size: 10rem;
+  }
+}
+</style>
--- src/routes/router.js
+++ src/routes/router.js
@@ -4,10 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Home from '../views/Home.vue';
+import PageNotFound from '../components/404.vue';
 
 const routes = new Router({
     routes: [
-        { path: '/', component: Home }
+        { path: '/', component: Home },
+        { path: '*', component: PageNotFound }
     ]
   })
 
