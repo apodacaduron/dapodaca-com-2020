89795ba18b998ac1286be42d39e4c8e62233936c
Daniel Apodaca <tanjiro@Daniels-MacBook.domain.name>
2020-10-15 07:40:41 -0600

    fix(router): redirect to landing

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,17 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 import Home from '../views/Home.vue';
-import PageNotFound from '../components/404.vue';
 
 const routes = new Router({
-    mode: 'history',
-    routes: [
-        { path: '/', component: Home },
-        { path: '*', component: PageNotFound }
-    ]
-  })
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '*', redirect: '/' },
+  ],
+});
 
 export default routes;
